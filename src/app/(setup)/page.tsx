b49830c532d7f8132b413ddd7dff5a028a51a5a5
b166er <delagneauadrien@yahoo.fr>
2025-02-17 11:57:52 +0100

    clerk init

---
+++ src/app/(setup)/page.tsx
@@ -0,0 +1,13 @@
+import { db } from "@/lib/db"; 
+import { redirect } from "next/navigation";
+
+export default async function SetupPage() {
+
+
+ 
+
+  return(
+   <div>setup page</div>
+  )
+  
+}
